fbff4d3016b820d92255901af8206df4c287c3d4
Conor Heins <conor.heins@gmail.com>
2025-12-11 10:38:46 +0100

    remove local finetuning script and sbatch call scripts from `scripts/`

--- scripts/start_docker_interactive.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/bash
-
-docker run -it --rm --gpus=\"device=$SLURM_STEP_GPUS\"\
-  -e HOME=$HOME \
-  -v $HOME:$HOME \
-  -v $(pwd):$(pwd) \
-  -w $(pwd) \
-  -u $(id -u):$(id -g) \
-  bllarse-dev "${@:-bash}"
